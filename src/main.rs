d9d2301447b84aaedd8100fb2aa21874aab08f84
VeyronSakai <yuukisakai123@gmail.com>
2021-05-04 21:31:48 +0900

    spawn_snake

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,31 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_plugins(DefaultPlugins).run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+    });
+}
+
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead);
+}
+
+// Tag Component of Snake Entity
+struct SnakeHead;
+
+// Resource materials of the snake
+struct Materials {
+    head_material: Handle<ColorMaterial>,
 }
